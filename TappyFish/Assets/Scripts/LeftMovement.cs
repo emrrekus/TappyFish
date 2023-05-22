510137f13a9294a83f8e3fa99139e9b8e38d1665
Emre <112270755+emrrekus@users.noreply.github.com>
2023-05-22 22:37:30 +0300

    aa

---
+++ TappyFish/Assets/Scripts/LeftMovement.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftMovement : MonoBehaviour
+{
+    public float speed;
+    private BoxCollider2D _boxCollider2D;
+    private float groundWidth;
+    private float obstacleWidth;
+
+    private void Start()
+    {
+
+        if (gameObject.CompareTag("Ground"))
+        {
+            _boxCollider2D = GetComponent<BoxCollider2D>();
+            groundWidth = _boxCollider2D.size.x;
+        }
+        else if (gameObject.CompareTag("Obstacle"))
+        {
+            obstacleWidth = GameObject.FindGameObjectWithTag("Column").GetComponent<BoxCollider2D>().size.x;
+        }
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+        if (gameObject.CompareTag("Ground"))
+        {
+            if (transform.position.x <= -groundWidth)
+            {
+                transform.position = new Vector2(transform.position.x + 2 * groundWidth, transform.position.y);
+            }
+        }else if (gameObject.CompareTag("Obstacle"))
+        {
+           
+        }
+
+       
+    }
+}
---
+++ TappyFish/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+
+    public GameObject obstacle;
+    public float maxTime;
+    public float maxY;
+    public float minY;
+    private float randomY;
+     float timer;
+
+    
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= maxTime)
+        {
+            randomY = Random.Range(minY, maxY);
+            InstantiateObstacle();
+            timer = 0;
+        }
+    }
+
+    public void InstantiateObstacle()
+    {
+        GameObject newObstacle = Instantiate(obstacle);
+        newObstacle.transform.position = new Vector2(transform.position.x, randomY);
+    }
+}
